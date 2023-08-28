99453eb1c214bcccbe2e744943b794b926719e3e
Sheyiemel <139079087+Sheyiemel@users.noreply.github.com>
2023-08-28 22:43:43 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
-  default     = "bits_bucket"
+  default     = "bitsbybucket"
 }
 
 variable "kms_key_alias" {
