04436652cc809cc84dfd17360c1a98432bfc14a4
Sheyiemel <139079087+Sheyiemel@users.noreply.github.com>
2023-08-29 19:31:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "bits_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.bits_key.arn
+        kms_master_key_id = aws_kms_key.bits_key.id
         sse_algorithm     = "aws:kms"
       }
     }
